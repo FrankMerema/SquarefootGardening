b685c4899cf2b00da135045b50622f97b033a48a
Frank Merema <frankmerema@gmail.com>
2021-03-19 10:55:15 +0100

    initial commit

---
+++ src/main/java/com/squarefoot/gardening/SquarefootGardening/SquarefootGardeningApplication.java
@@ -0,0 +1,13 @@
+package com.squarefoot.gardening.SquarefootGardening;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquarefootGardeningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquarefootGardeningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/squarefoot/gardening/SquarefootGardening/SquarefootGardeningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squarefoot.gardening.SquarefootGardening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquarefootGardeningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
