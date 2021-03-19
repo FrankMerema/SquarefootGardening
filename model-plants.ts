f14398d0dfb0b4926e55d674ecec0131e7a3da62
Albert Groothedde <albertgroothedde@gmail.com>
2021-03-19 12:00:42 +0100

    Update model-plants

--- model-plants.ts
+++ model-plants.ts
@@ -7,6 +7,6 @@ type json = {
 type Plant = {
   sowWeeks: number[]; // 52 weken
   harvestWeeks: number[];
-  detestedNeighbor: Plant;
-  preferredNeighbor: Plant;
+  detestedNeighbor: Plant[];
+  preferredNeighbor: Plant[];
 };
