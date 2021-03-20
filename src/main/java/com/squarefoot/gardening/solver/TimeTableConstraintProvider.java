89d85bc05753ca8587863dba5eb15490e251c500
Albert Groothedde <albertgroothedde@gmail.com>
2021-03-20 21:24:02 +0100

    Adds `squareConflict` with equal square, and overlapping start/end week

--- src/main/java/com/squarefoot/gardening/solver/TimeTableConstraintProvider.java
+++ src/main/java/com/squarefoot/gardening/solver/TimeTableConstraintProvider.java
@@ -6,7 +6,9 @@ import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
 import org.optaplanner.core.api.score.stream.Constraint;
 import org.optaplanner.core.api.score.stream.ConstraintFactory;
 import org.optaplanner.core.api.score.stream.ConstraintProvider;
-import org.optaplanner.core.api.score.stream.Joiners;
+import static org.optaplanner.core.api.score.stream.Joiners.equal;
+import static org.optaplanner.core.api.score.stream.Joiners.overlapping;
+import static org.optaplanner.core.api.score.stream.Joiners.lessThan;
 
 public class TimeTableConstraintProvider implements ConstraintProvider {
 
@@ -14,26 +16,24 @@ public class TimeTableConstraintProvider implements ConstraintProvider {
         public Constraint[] defineConstraints(ConstraintFactory constraintFactory) {
                 return new Constraint[] {
                                 // Hard constraints
-                                square(constraintFactory),
+                                squareConflict(constraintFactory),
                                 // teacherConflict(constraintFactory),
                                 // studentGroupConflict(constraintFactory),
                                 // Soft constraints are only implemented in the "complete" implementation
                 };
         }
 
-        private Constraint square(ConstraintFactory constraintFactory) {
+        private Constraint squareConflict(ConstraintFactory factory) {
                 // A square can accommodate at most one plantLocation at the same time.
 
-                // Select a lesson ...
-                return constraintFactory.from(PlantLocation.class)
-                                // ... and pair it with another lesson ...
-                                .join(PlantLocation.class,
-                                                // ... in the same timeslot ...
-                                                Joiners.equal(PlantLocation::getStartWeek),
-                                                // ... in the same room ...
-                                                Joiners.equal(PlantLocation::getSerializedSquare),
-                                                // ... and the pair is unique (different id, no reverse pairs)
-                                                Joiners.lessThan(PlantLocation::getId))
+                // Select a PlantLocation pair
+                return factory.fromUniquePair(PlantLocation.class,
+                                // ... in the same square ...
+                                equal(PlantLocation::getSerializedSquare),
+                                // ... overlapping in the square ...
+                                overlapping(t -> t.getStartWeek(), t -> t.getEndWeek()),
+                                // ... and the pair is unique (different id, no reverse pairs)
+                                lessThan(PlantLocation::getId))
                                 // then penalize each pair with a hard weight.
                                 .penalize("Square conflict", HardSoftScore.ONE_HARD);
         }
